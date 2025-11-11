39388be6bee4510d12fe10a1aa556409acaa10b9
massinissa mameri <massilockwood93@gmail.com>
2025-11-11 00:53:05 -0500

    ajout de 2 container stylisee

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,40 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
           child: Column(
             children: [
               Text('hello world'),
               Text('bienvenue dans mon atelier'),
+              Container(
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(12),
+                ),
+                child: Text('boite stylisee avec container',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+                )),
+                Container(
+                  margin: EdgeInsets.all(25),
+                 padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(197, 10, 84, 3),
+                    borderRadius: BorderRadius.circular(11)),
+                    child: Text('my second box',
+                    style: TextStyle(color: const Color.fromARGB(255, 252, 252, 252), fontSize: 17)),
+                )
             ],
           ),
-        ),
+        ), 
       ),
     );
-  } 
+  }
 }
+        
+       
+    
   
