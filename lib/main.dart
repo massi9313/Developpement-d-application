3a642fe6f079aeb350df6ad65a93c5f20d62fa97
massinissa mameri <massilockwood93@gmail.com>
2025-11-11 03:00:36 -0500

    personaliser mon interface mais a la fin je remet tout en place

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,8 @@ class MainApp extends StatelessWidget {
                ),
 
                 Container(
-                  margin: EdgeInsets.all(25),  
-                 padding: EdgeInsets.all(15),
+                  margin: EdgeInsetsGeometry.all(25),  
+                 padding: EdgeInsetsGeometry.all(15),
                   decoration: BoxDecoration(
                     color: const Color.fromARGB(197, 10, 84, 3),
                     borderRadius: BorderRadius.circular(11)),
@@ -43,29 +43,29 @@ class MainApp extends StatelessWidget {
                       alignment: Alignment.center,
                       children: [
                         Container(width: 200, height: 200, color: Colors.blue,),
-                        Icon(Icons.star, size: 100, color: Colors.amber),
+                        Icon(Icons.star, size: 100, color: const Color.fromARGB(255, 210, 105, 7)),
                         Text("superpose !",
                         style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
                       ],
                     ),
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Icon(Icons.home, color: Colors.blue, size: 40),
-                        Icon(Icons.star, color: Colors.amber, size: 40),
-                        Icon(Icons.settings,color: Colors.grey, size: 40),
-                        Icon(Icons.phone, color: Colors.green, size: 40,)
-                      ],
-                    ),
-                    Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Text('titre pricipal', style: TextStyle(fontSize: 24)),
+                        Icon(Icons.home, color: const Color.fromARGB(255, 49, 32, 123), size: 40),
+                        Icon(Icons.star, color: const Color.fromARGB(255, 171, 129, 5), size: 40),
+                        Icon(Icons.settings,color: const Color.fromARGB(255, 168, 148, 148), size: 40),
+                        Icon(Icons.phone, color: const Color.fromARGB(255, 3, 61, 5), size: 40,)
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('titre pricipal', style: TextStyle(fontSize: 28)),
                         SizedBox(height: 20),
-                        Text('sous-titre ou description', style: TextStyle(fontSize: 16)),
-                        Icon(Icons.thumb_up, color: Colors.green, size: 40),
+                        Text('sous-titre ou description', style: TextStyle(fontSize: 20)),
+                        Icon(Icons.thumb_up, color: const Color.fromARGB(255, 4, 34, 143), size: 48),
                       ],
                     )
             ],
